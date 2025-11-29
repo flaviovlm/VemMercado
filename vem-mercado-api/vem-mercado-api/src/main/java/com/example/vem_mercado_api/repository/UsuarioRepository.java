4332e4ef3accae8ce344bb4d463eca331df39a83
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-11-28 21:12:12 -0300

    Finalizando Models e Repository

--- vem-mercado-api/vem-mercado-api/src/main/java/com/example/vem_mercado_api/repository/UsuarioRepository.java
+++ vem-mercado-api/vem-mercado-api/src/main/java/com/example/vem_mercado_api/repository/UsuarioRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<UsuarioModel , Long> {
     Optional<UsuarioModel> findByEmail(String email);
     Optional<UsuarioModel> findByCpf(String cpf);
+    Optional<UsuarioModel> findByTelefone(String telefone);
 }
