f6489a4aaec2f1b62c68a1e6bc66ee85619d8ba5
Nicolas Roger Araujo Monteiro da  Silva <nicolas@MacBook-Air-de-Nicolas.local>
2025-11-29 18:57:48 -0300

    finalizando ProductResponseDTO

--- vem-mercado-api/src/main/java/api/dto/produtoDTO/ProdutoResponseDTO.java
+++ vem-mercado-api/src/main/java/api/dto/produtoDTO/ProdutoResponseDTO.java
@@ -1,4 +1,55 @@
 package api.dto.produtoDTO;
 
+import api.model.produto.Categoria;
+
+import java.math.BigDecimal;
+
 public class ProdutoResponseDTO {
+
+    private String nome;
+    private String descricao;
+    private BigDecimal valor;
+    private Categoria categoria;
+
+    public ProdutoResponseDTO() {
+    }
+
+    public ProdutoResponseDTO(String nome, String descricao, BigDecimal valor, Categoria categoria) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.categoria = categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
