242d91bbec094a8d80036d9a611290bb53264a7a
Nicolas Roger Araujo Monteiro da  Silva <nicolas@MacBook-Air-de-Nicolas.local>
2025-11-29 21:38:09 -0300

    adicionando valorTotal e status em PedidoResponseDTO

--- vem-mercado-api/src/main/java/api/dto/pedidoDTO/PedidoResponseDTO.java
+++ vem-mercado-api/src/main/java/api/dto/pedidoDTO/PedidoResponseDTO.java
@@ -1,22 +1,38 @@
 package api.dto.pedidoDTO;
 
 import api.dto.itemPedidoDTO.ItemPedidoRequestDTO;
+import api.model.pedido.Status;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class PedidoResponseDTO {
 
+    private Long idPedido;
     private Long usuarioId;
     private List<ItemPedidoRequestDTO> itens;
     private Long enderecoEntregaId;
+    private Status status;
+    private BigDecimal valorTotal;
 
     public PedidoResponseDTO() {
     }
 
-    public PedidoResponseDTO(Long usuarioId, List<ItemPedidoRequestDTO> itens, Long enderecoEntregaId) {
+    public PedidoResponseDTO(Long idPedido, Long usuarioId, List<ItemPedidoRequestDTO> itens, Long enderecoEntregaId, Status status, BigDecimal valorTotal) {
+        this.idPedido = idPedido;
         this.usuarioId = usuarioId;
         this.itens = itens;
         this.enderecoEntregaId = enderecoEntregaId;
+        this.status = status;
+        this.valorTotal = valorTotal;
+    }
+
+    public Long getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(Long idPedido) {
+        this.idPedido = idPedido;
     }
 
     public Long getUsuarioId() {
@@ -41,5 +57,21 @@ public class PedidoResponseDTO {
 
     public void setEnderecoEntregaId(Long enderecoEntregaId) {
         this.enderecoEntregaId = enderecoEntregaId;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
     }
 }
