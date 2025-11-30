07b4f4d4f9048c1838590addfbcc22556e3884cd
Nicolas Roger Araujo Monteiro da  Silva <nicolas@MacBook-Air-de-Nicolas.local>
2025-11-30 17:56:42 -0300

    finalizando a criação de ProdutoService

--- vem-mercado-api/src/main/java/api/repository/ProdutoRepository.java
+++ vem-mercado-api/src/main/java/api/repository/ProdutoRepository.java
@@ -4,7 +4,9 @@ import api.model.produto.ProdutoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProdutoRepository extends JpaRepository<ProdutoModel , Long> {
     List<ProdutoModel> findByNomeContainingIgnoreCase(String nome);
+    Optional<ProdutoModel> findByEan(String ean);
 }
