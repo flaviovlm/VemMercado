d41e4ca258514a1ffc6e1ecb4c51a78d6c11abc4
Guilherme <guifefo@gmail.com>
2025-12-01 22:30:11 -0300

    Classe UsuarioLoginRequestDTO criada

---
+++ vem-mercado-api/src/main/java/api/dto/usuarioDTO/UsuarioLoginRequestDTO.java
@@ -0,0 +1,40 @@
+package api.dto.usuarioDTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UsuarioLoginRequestDTO {
+
+    @NotBlank(message = "O email é obrigatorio")
+    @Email(message = "Informe um email valido")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatoria")
+    @Size(min = 3 , max = 10 , message = "A senha deve ter minimo 10 caracteres")
+    private String senha;
+
+    public UsuarioLoginRequestDTO() {
+    }
+
+    public UsuarioLoginRequestDTO(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public @NotBlank(message = "O email é obrigatorio") @Email(message = "Informe um email valido") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "O email é obrigatorio") @Email(message = "Informe um email valido") String email) {
+        this.email = email;
+    }
+
+    public @NotBlank(message = "A senha é obrigatoria") @Size(min = 3, max = 10, message = "A senha deve ter minimo 10 caracteres") String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(@NotBlank(message = "A senha é obrigatoria") @Size(min = 3, max = 10, message = "A senha deve ter minimo 10 caracteres") String senha) {
+        this.senha = senha;
+    }
+}
